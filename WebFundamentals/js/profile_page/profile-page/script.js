dacc4ad06c1ffd889fbded71d29b9af907f008d8
skendaj <skendajbruno07@gmail.com>
2023-04-01 01:10:54 +0200

    completed

--- WebFundamentals/js/profile_page/profile-page/script.js
+++ WebFundamentals/js/profile_page/profile-page/script.js
@@ -1,8 +1,23 @@
-function changeName() {
-    document.getElementById("name").innerHTML = "Bruno Skendaj"
+// exercise 1 changes
+function changeName() { 
+    document.getElementById("name").innerHTML = "Bruno Skendaj";
 }
 
-function actionRequests () {
-    document.getElementById("request")
-    request.remove()
+function actionsOfRequests() {
+// exercise 2
+    document.getElementById("request");
+    request.remove();
+// exercise 3 Bonus
+    var num=2;
+    num--;
+    var DecreaseNum = document.getElementById("crNumber");
+    console.log(num)
+    document.getElementById("crNumber").innerHTML = num;
+// exercise 4 Bonus
+    var num1=500;
+    num1++;
+    var IncreaseNum = document.getElementById("plusConnectionNum");
+    console.log(num1)
+    document.getElementById("plusConnectionNum").innerHTML = num1;
+
 }
