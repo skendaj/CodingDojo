62b1081a3a73e983a20b0a45e967e44069cc91ec
skendaj <skendajbruno07@gmail.com>
2023-04-12 23:03:07 +0200

    completed

---
+++ MERN/object_master/tempCodeRunnerFile.js
@@ -0,0 +1,8 @@
+const pokemonIdsDv3 = pokemon.filter( p => p % 3 == 0);
+// console.log(pokemonIdsDv3);
+
+// const pkmnIds = pokemon.map(p => p.id);
+// console.log(pokemon);
+
+// const bListPkmn = pokemon.filter( p => p.name[0] === "B" );
+// console.log(bListPkmn);
